241d11e4634de15eb2b2077d81fa46e8306a62bc
wonmimi <devwm@naver.com>
2021-06-16 18:50:04 +0900

    feat: ch02 > 생성자

--- src/ch02/sub04/StudentTest.java
+++ src/ch02/sub04/StudentTest.java
@@ -3,15 +3,20 @@ package ch02.sub04;
 public class StudentTest {
     public static void main(String[] args) {
         Student studentPark = new Student(); // student 객체인 studentLee 인스턴스를 참조변수로 생성
+
         studentPark.studentId = 12345;
         studentPark.setStudentName("박씨");
         studentPark.address = "서울 한남동";
         studentPark.showStudentInfo();
 
         Student studentJung  = new Student();
+
         studentJung.studentId = 67890;
         studentJung.studentName = "정씨";
         studentJung.address = "경기 부천";
         studentJung.showStudentInfo();
+
+        System.out.println(studentPark);
+        System.out.println(studentJung);
     }
 }
---
+++ src/ch06/StudentTest.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        System.out.println(studentLee.showStudentInfo());
+
+        Student studentJung = new Student(12345, "정씨",29);
+        System.out.println(studentJung.showStudentInfo());
+
+
+    }
+}
