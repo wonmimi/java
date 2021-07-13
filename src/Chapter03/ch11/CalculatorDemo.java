02ee74e7c2a77a328b7225558dccd1a3c663fb85
wonmimi <devwm@naver.com>
2021-07-14 08:35:55 +0900

    chap03 - interface

---
+++ src/Chapter03/ch11/CalculatorDemo.java
@@ -0,0 +1,21 @@
+package Chapter03.ch11;
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+
+
+        int num1 = (int) Calc.PI;
+        int num2 = 2;
+        // Calc Type 으로 구현한 CompleteCalc
+        Calc calc = new CompleteCalc();
+        System.out.println(calc.add(num1,num2));
+        System.out.println(calc.substract(num1,num2));
+        System.out.println(calc.times(num1,num2));
+        System.out.println(calc.devide(num1,num2));
+
+        CompleteCalc calc2 = new CompleteCalc();
+        calc2.showInfo();
+
+
+    }
+}
---
+++ src/Chapter03/ch11/CompleteCalc.java
@@ -0,0 +1,20 @@
+package Chapter03.ch11;
+
+public class CompleteCalc extends Calculator{
+
+    @Override
+    public int times(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int devide(int num1, int num2) {
+        if (num2 == 0)
+            return ERROR;
+        return num1 / num2;
+    }
+
+    public void showInfo(){
+        System.out.println("구현 완료");
+    }
+}
