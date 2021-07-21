d62a1fe76931eda4fda05eedb475a7c9fd888552
wonmimi <devwm@naver.com>
2021-07-21 23:38:32 +0900

    docs: array , 객체 배열

---
+++ src/Chapter02/ch20/ArrayTest.java
@@ -0,0 +1,21 @@
+package Chapter02.ch20;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int total = 0;
+
+        for (int i = 0 , num=1; i<arr.length; i++ ){
+//            System.out.println(arr[i]);
+            arr[i] = num++;
+        }
+//        = int[] arr = new int[]{1,2,3,4,5,6,7,8,9,10};
+
+        for(int value : arr){
+            total += value;
+        }
+
+        System.out.println(total);
+
+    }
+}
---
+++ src/Chapter02/ch21/ObjectCopyTest.java
@@ -0,0 +1,57 @@
+package Chapter02.ch21;
+
+public class ObjectCopyTest {
+    public static void main(String[] args) {
+        Book[] library = new Book[5];
+        Book[] copyLibrary = new Book[5];
+
+
+        library[0] = new Book("책1","작가1");
+        library[1] = new Book("책2","작가2");
+        library[2] = new Book("책3","작가3");
+        library[3] = new Book("책4","작가4");
+        library[4] = new Book("책5","작가5");
+
+
+        // 얕은 복사
+//        System.arraycopy(library,0,copyLibrary,0,5);
+
+
+        // 깊은 복사
+        copyLibrary[0] = new Book();
+        copyLibrary[1] = new Book();
+        copyLibrary[2] = new Book();
+        copyLibrary[3] = new Book();
+        copyLibrary[4] = new Book();
+
+        for(int i = 0 ; i < library.length; i++){
+            copyLibrary[i].setTitle(library[i].getTitle());
+            copyLibrary[i].setAuthor(library[i].getAuthor());
+        }
+
+
+        for (Book book : library){
+            System.out.println(book);
+            book.showInfo();
+        }
+
+        // 값 변경
+        library[0].setAuthor("작가1 변경");
+        library[0].setTitle(("책1 변경"));
+
+        System.out.println("- - - - - - - - - - ");
+        for (Book book : copyLibrary){
+            System.out.println(book);
+            book.showInfo();
+        }
+
+        System.out.println("- - - - - CHANGE- - - - - ");
+
+        for (Book book : library){
+            System.out.println(book);
+            book.showInfo();
+        }
+
+
+    }
+}
