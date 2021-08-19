802fb5b1906fca1cfc5023b19af1dd79f8fd3c72
wonmimi <devwm@naver.com>
2021-08-20 03:59:12 +0900

    Stack & Queue 구현

---
+++ src/Algorithm/QueuePractice.java
@@ -0,0 +1,27 @@
+package Algorithm;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueuePractice {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(1);
+        queue.offer(10);
+        queue.remove(); // null일경우 error (NoSuchElementException)
+        queue.offer(7);
+        queue.poll(); // null일경우 null 리턴
+        queue.offer(3);
+        queue.offer(99);
+        queue.offer(2);
+        queue.offer(1);
+
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.peek());
+            queue.poll();
+//            System.out.println(queue.poll());
+        }
+    }
+}
---
+++ src/Algorithm/StackPractice.java
@@ -0,0 +1,30 @@
+package Algorithm;
+
+import java.util.Stack;
+
+public class StackPractice {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        stack.push(5);
+        stack.push(2);
+        stack.push(6);
+        stack.pop();
+        stack.push(7);
+        System.out.println("top : "+stack.peek()); // top
+        System.out.println("= = search = = ");
+        System.out.println(stack.search(7)); // 1
+        System.out.println(stack.search(9)); // -1
+        System.out.println(stack.search(2)); // 2 (첫번쨰로 찾은)
+        System.out.println(stack.search(1)); // 5
+        System.out.println("= = pop = = ");
+        while (!stack.empty()){
+            System.out.println(stack.pop());
+        }
+
+
+    }
+}
